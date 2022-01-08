85808008f8b635a71544a6ab88955d22b7b8d0d6
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2022-01-08 14:05:55 +0200

    Add categories...

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BLL.Interfaces;
+using BLL.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAPI.Logs;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/category/")]
+    [ApiController]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoriesService _categoriesService;
+        private readonly ILogger<CategoriesController> _logger;
+
+        public CategoriesController(ICategoriesService categoriesService, ILogger<CategoriesController> logger)
+        {
+            _categoriesService = categoriesService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult<IEnumerable<CategoryModel>> GetAll()
+        {
+            try
+            {
+                var result = _categoriesService.GetAll();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogInfoMethod(ex, _logger);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<CategoryModel>> GetById(int id)
+        {
+            try
+            {
+                var result = await _categoriesService.GetByIdAsync(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogInfoMethod(ex, _logger);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}/lots")]
+        [AllowAnonymous]
+        public ActionResult<IEnumerable<LotModel>> GetLotByCategoryId(int id)
+        {
+            try
+            {
+                var result = _categoriesService.GetLotByCategoryId(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogInfoMethod(ex, _logger);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("create")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Add([FromBody] CategoryModel model)
+        {
+            try
+            {
+                await _categoriesService.AddAsync(model);
+                return CreatedAtAction(nameof(GetById), new { id = model.Id }, model);
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogInfoMethod(ex, _logger);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("update")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Update(CategoryModel categoryModel)
+        {
+            try
+            {
+                await _categoriesService.UpdateAsync(categoryModel);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogInfoMethod(ex, _logger);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("remove/{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            try
+            {
+                await _categoriesService.DeleteByIdAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                LogInfo.LogInfoMethod(ex, _logger);
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
