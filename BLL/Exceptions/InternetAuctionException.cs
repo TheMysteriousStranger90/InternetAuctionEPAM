b1c590f3688adfc60f5e804288b768627070c725
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2022-01-08 09:53:33 +0200

    Add exceptions...

---
+++ BLL/Exceptions/InternetAuctionException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BLL.Exceptions
+{
+    [Serializable]
+    public class InternetAuctionException : Exception
+    {
+        private static readonly string DefaultMessage = "Internet Auction exception was thrown.";
+
+        public InternetAuctionException() : base(DefaultMessage)
+        {
+
+        }
+
+        public InternetAuctionException(string message) : base(message)
+        {
+
+        }
+
+        public InternetAuctionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected InternetAuctionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
