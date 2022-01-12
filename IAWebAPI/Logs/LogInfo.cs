8b3542353db38d9661c0ba7d68abce3e1c713327
Bohdan Harabadzhyu <bohdan.harabadzhyu@yandex.ru>
2022-01-12 10:02:27 +0200

    Add Logs...

---
+++ IAWebAPI/Logs/LogInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Logs
+{
+    public static class LogInfo
+    {
+        public static void LogInfoMethod(Exception exception, ILogger logger)
+        {
+            logger.LogInformation("Message: " + exception.Message);
+            logger.LogInformation("In method: " + exception.TargetSite);
+        }
+    }
+}
